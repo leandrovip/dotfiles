d3b5f1b8730a440c4d017217a5b8d65bae8677ef
Leandro Ferreira <leandro@vipsolucoes.com>
2022-08-19 10:49:36 -0300

    update powershell nuget profile

--- NuGet_profile.ps1
+++ NuGet_profile.ps1
@@ -1,31 +1,61 @@
 function mig-erp
-{
-    [CmdletBinding(DefaultParameterSetName='Name')]
-    Param
-    (
-         [Parameter(Mandatory=$true, Position=0, ParameterSetName='Name')]
-         [string] $Name
-    )
-	
-	Add-Migration -ConfigurationTypeName VipERP.Data.Migrations.ERP.Configuration $Name
+{	
+	Add-Migration -ConfigurationTypeName VipERP.Data.Migrations.ERP.Configuration 'Inicial'
 }
 
 function mig-pdv
-{
-    [CmdletBinding(DefaultParameterSetName='Name')]
-    Param
-    (
-         [Parameter(Mandatory=$true, Position=0, ParameterSetName='Name')]
-         [string] $Name
-    )
+{	
+	Add-Migration -ConfigurationTypeName VipERP.Data.Migrations.PDV.Configuration 'Inicial'
+}
+
+function temp-erp
+{	
+	Add-Migration -ConfigurationTypeName VipERP.Data.Migrations.ERP.Configuration 'Temp'
+}
+
+function temp-pdv
+{	
+	Add-Migration -ConfigurationTypeName VipERP.Data.Migrations.PDV.Configuration 'Temp'
+}
+
+function script-erp {
+    update-database -ConfigurationTypeName VipERP.Data.Migrations.ERP.Configuration -Script -SourceMigration:0
+}
+
+function script-pdv {
+    update-database -ConfigurationTypeName VipERP.Data.Migrations.PDV.Configuration -Script -SourceMigration:0
+}
+
+## BACKUP ##
+
+# function bkp-mig-erp
+# {
+#     [CmdletBinding(DefaultParameterSetName='Name')]
+#     Param
+#     (
+#          [Parameter(Mandatory=$true, Position=0, ParameterSetName='Name')]
+#          [string] $Name
+#     )
 	
-	Add-Migration -ConfigurationTypeName VipERP.Data.Migrations.PDV.Configuration $Name
-}
+# 	Add-Migration -ConfigurationTypeName VipERP.Data.Migrations.ERP.Configuration $Name
+# }
 
-function up-erp {
-	Update-Database -ConfigurationTypeName VipERP.Data.Migrations.ERP.Configuration
-}
+# function bkp-mig-pdv
+# {
+#     [CmdletBinding(DefaultParameterSetName='Name')]
+#     Param
+#     (
+#          [Parameter(Mandatory=$true, Position=0, ParameterSetName='Name')]
+#          [string] $Name
+#     )
+	
+# 	Add-Migration -ConfigurationTypeName VipERP.Data.Migrations.PDV.Configuration $Name
+# }
 
-function up-pdv {
-	Update-Database -ConfigurationTypeName VipERP.Data.Migrations.PDV.Configuration
-}
+# function up-erp {
+# 	Update-Database -ConfigurationTypeName VipERP.Data.Migrations.ERP.Configuration
+# }
+
+# function up-pdv {
+# 	Update-Database -ConfigurationTypeName VipERP.Data.Migrations.PDV.Configuration
+# }
