b9ddbaa1cf2b4c0eff29fa35652962a5ac9e7aa2
Leandro Ferreira <leandro@vipsolucoes.com>
2023-10-22 17:00:44 -0300

    Update dotfiles

--- terminal/Microsoft.PowerShell_profile.ps1
+++ terminal/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,6 @@
  #Profile config - User: Leandro User E-mail: leandro@vipsolucoes.com
 
 $env:LC_ALL='C.UTF-8'
-$MaximumHistoryCount = 20000
-
-# Import Modules
-Import-Module posh-git
-Import-Module Terminal-Icons
-Import-Module PsReadLine
 
 # Functions
 function Add-Alias($name, $alias) {
@@ -21,22 +15,32 @@ function global:$name {
     $func | iex
 }
 
-function viperp { set-location "E:\dev\VipERP" }
-function sgi { set-location "E:\dev\Vip.SGI" }
-function mobile { set-location "E:\dev\VipERP.Mobile" }
-function migration { Invoke-Item "E:\dev\VipERP.Migrations\src\VipERP.Migrations\bin\Release\net6.0-windows\VipERP.Migrations.exe" }
-function emul { Invoke-Item "D:\Programas\scrcpy\scrcpy-noconsole.vbs" }
+function viperp {
+	set-location "D:\dev\VipERP"
+}
 
-function dev { set-location "E:\dev" }
-function temp { Set-Location "E:\temp" }
-function exp { Invoke-Item "." }
-function la() { Get-ChildItem | Format-Wide }
-function tail { Get-Content $args -Tail 30 -Wait }
-function title { $Host.UI.RawUI.WindowTitle = "$pwd" }
+function migration {
+    Invoke-Item "D:\dev\VipERP.Migrations\src\VipERP.Migrations\bin\Release\net6.0-windows\VipERP.Migrations.exe"
+}
 
-function take {
-  New-Item -ItemType directory $args
-  Set-Location "$args"
+function mobile {
+	set-location "D:\dev\VipERP.Mobile"
+}
+
+function sgi {
+	set-location "D:\dev\Vip.SGI"
+}
+
+function dev {
+	set-location "D:\dev"
+}
+
+function temp {
+    Set-Location "D:\temp"
+}
+
+function exp {
+    Invoke-Item "."
 }
 
 function mssql {
@@ -44,46 +48,32 @@ function mssql {
     Invoke-Expression "net start $mssqlservice"
 }
 
-function pushgit {
-    $branch = $(git rev-parse --abbrev-ref HEAD)
-    git push --set-upstream origin $branch
+function emul {
+    Invoke-Item "D:\Programas\scrcpy\scrcpy-noconsole.vbs"
 }
 
-# Custom aliases
+# Git aliases
 Add-Alias ab 'git ckb'
 Add-Alias am 'git ck master'
 Add-Alias ad 'git ck develop'
 Add-Alias au 'git up'
 Add-Alias al 'git l'
 Add-Alias as 'git st'
-Add-Alias ngs 'ng serve'
 
-# Oh-My-Posh
+function pushgit {
+    $branch = $(git rev-parse --abbrev-ref HEAD)
+    git push --set-upstream origin $branch
+}
+
+# Git Posh
+Import-Module posh-git
+
 oh-my-posh init pwsh --config 'D:\Users\Leandro\AppData\Local\Programs\oh-my-posh\themes\takuya.omp.json' | Invoke-Expression
+
+Import-Module Terminal-Icons
 
 # Tab completion
 Set-PSReadlineKeyHandler -Chord 'Shift+Tab' -Function Complete
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
-# History definitions
-$HistoryFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
-Register-EngineEvent PowerShell.Exiting -Action { Get-History | Export-Clixml $HistoryFilePath } | out-null
-if (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
-
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
-Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-
-Set-PSReadLineOption -ShowToolTips
-Set-PSReadLineOption -PredictionSource History
-
-# Aliases
-Set-Alias which Get-Command
-Set-Alias open Invoke-Item
-
-# Compute file hashes - useful for checking successful downloads
-function md5    { Get-FileHash -Algorithm MD5 $args }
-function sha1   { Get-FileHash -Algorithm SHA1 $args }
-function sha256 { Get-FileHash -Algorithm SHA256 $args }
 
 Clear-Host
